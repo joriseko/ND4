e5fc21ccdf8e4e76d33a82072ef2894244f01420
joris_sekonas <joris.sekonas@gmail.com>
2020-04-18 08:52:48 +0300

    no message

---
+++ ND4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ND4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
